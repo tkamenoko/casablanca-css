f643c0e10bb2185392c564b0068eae2844110de9
T. Kameyama <tkamenoko@vivaldi.net>
2023-10-21 14:50:13 +0900

    add: styled example

--- testbed/vite-react-ts/vite.config.ts
+++ testbed/vite-react-ts/vite.config.ts
@@ -1,11 +1,12 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import { macrostyles } from '@macrostyles/core/vite';
+import { macrostylesReact } from '@macrostyles/react/vite';
 import postcssNested from 'postcss-nested';
 
 export default defineConfig({
   css: {
     postcss: { plugins: [postcssNested()] },
   },
-  plugins: [react(), macrostyles()],
+  plugins: [react(), macrostylesReact(), macrostyles()],
 });
