ea5d41e6fe9c5faf0a0017d25371204798a29d88
T. Kameyama <tkamenoko@vivaldi.net>
2023-11-11 04:25:23 +0900

    rm: old testbed projects

--- testbed/rakkas/vite.config.ts
+++
@@ -1,9 +0,0 @@
-import { defineConfig } from 'vite';
-import rakkas from 'rakkasjs/vite-plugin';
-import { macrostyles } from '@macrostyles/core/vite';
-import tsconfigPaths from 'vite-tsconfig-paths';
-import { macrostylesReact } from '@macrostyles/react/vite';
-
-export default defineConfig({
-  plugins: [tsconfigPaths(), macrostylesReact(), macrostyles(), rakkas()],
-});
