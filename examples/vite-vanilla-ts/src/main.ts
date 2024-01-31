461b122f57ae86773f3a161958ff6637c039d1fb
T. Kameyama <tkamenoko@vivaldi.net>
2024-01-31 22:35:20 +0900

    fix: example to use global style

--- examples/vite-vanilla-ts/src/main.ts
+++ examples/vite-vanilla-ts/src/main.ts
@@ -1,11 +1,19 @@
-import { css } from '@macrostyles/core';
+import { css, injectGlobal } from '@macrostyles/core';
 import { modularScale } from 'polished';
 
 import { grid, textAlign } from './styles';
 
+injectGlobal`
+  * {
+    margin: 0;
+    box-sizing: border-box;
+  }
+`;
+
 const h1Style = css`
   color: red;
   font-size: ${modularScale(4)};
+  border: 1px solid red;
 `;
 
 const divStyle = css`
