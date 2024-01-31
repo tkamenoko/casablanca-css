cc2c2d9c3f7a2e2148eb5a9d9d448d95818bb12e
T. Kameyama <tkamenoko@vivaldi.net>
2024-02-01 00:29:06 +0900

    add: examples

--- examples/vite-react-ts/src/main.tsx
+++ examples/vite-react-ts/src/main.tsx
@@ -1,7 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { injectGlobal } from "@macrostyles/core";
 
 import { App } from './App';
+
+injectGlobal`
+  * {
+    margin: 0;
+    box-sizing: border-box;
+  }
+`;
+
 
 const rootElement = document.getElementById('root');
 
