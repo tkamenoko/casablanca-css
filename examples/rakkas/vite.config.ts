1b03dcc745119ba74875c762968455ab2e6b24da
T.Kameyama <tkamenoko@vivaldi.net>
2024-02-27 01:01:31 +0900

    fix: rakkas requires @vitejs/plugin-react manually (#13)

--- examples/rakkas/vite.config.ts
+++ examples/rakkas/vite.config.ts
@@ -1,9 +1,16 @@
 import { casablanca } from "@casablanca/core/vite";
 import { casablancaReact } from "@casablanca/react/vite";
+import react from "@vitejs/plugin-react";
 import rakkas from "rakkasjs/vite-plugin";
 import { defineConfig } from "vite";
 import tsconfigPaths from "vite-tsconfig-paths";
 
 export default defineConfig({
-  plugins: [tsconfigPaths(), casablancaReact(), casablanca(), rakkas()],
+  plugins: [
+    tsconfigPaths(),
+    react(),
+    casablancaReact(),
+    casablanca(),
+    rakkas(),
+  ],
 });
