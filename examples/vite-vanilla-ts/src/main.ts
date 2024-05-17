a874c6c8d8c81321db44f48e9edac9173b5a574b
T. Kameyama <tkamenoko@vivaldi.net>
2024-05-18 00:40:07 +0900

    fix: example [WIP]

--- examples/vite-vanilla-ts/src/main.ts
+++ examples/vite-vanilla-ts/src/main.ts
@@ -12,15 +12,23 @@ injectGlobal`
 const h1Style = css`
   padding: .5rem 1rem;
   background-color: ${openProps.gray0};
-  color: ${openProps.teal4};
+  color: ${openProps.red5};
   font-size: ${openProps.fontSize6};
-  border: ${openProps.borderSize3} solid currentColor;
+  border: ${openProps.borderSize3} solid ${openProps.purple4};
   border-radius: ${openProps.radius3};
+`;
+
+const casablancaSpan = css`
+  color: ${openProps.teal4};
+`;
+
+const tsSpan = css`
+  color: ${openProps.blue4};
 `;
 
 const divStyle: string = css`
   min-height: 100svh;
-  background-image: ${openProps.gradient14};
+  background-image: ${openProps.gradient17};
   composes: ${grid};
   padding: .5rem 0;
   grid-template-columns: auto;
@@ -31,7 +39,7 @@ const divStyle: string = css`
   & .${textAlign} {
     font-size: ${openProps.fontSize4};
     font-weight: ${openProps.fontWeight7};
-    color: ${openProps.gray7};
+    color: ${openProps.gray1};
   }
 `;
 
@@ -39,7 +47,9 @@ const app = document.querySelector<HTMLDivElement>("#app");
 if (app) {
   app.innerHTML = /* html */ `
   <div class="${divStyle}">
-    <h1 class="${h1Style}">Casablanca + Vanilla-TS</h1>
+    <h1 class="${h1Style}">
+      <span class="${casablancaSpan}">Casablanca</span> + <span class="${tsSpan}">Vanilla-TS</span>
+    </h1>
     <p class="${textAlign}">
       Zero runtime CSS-in-JS for vite.
     </p>
