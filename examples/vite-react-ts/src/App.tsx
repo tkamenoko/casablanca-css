96abb6798c1f43ae8f6567864271a60636df301c
T. Kameyama <tkamenoko@vivaldi.net>
2024-05-28 18:23:25 +0900

    fix: react example

--- examples/vite-react-ts/src/App.tsx
+++ examples/vite-react-ts/src/App.tsx
@@ -2,58 +2,120 @@ import { css } from "@casablanca/core";
 import { styled } from "@casablanca/styled";
 import openProps from "open-props";
 import { type FC, useState } from "react";
-import { Button } from "./Button";
+import { Counter } from "./Counter";
 
-const StyledButton = styled(Button)`
-  display: block;
-  border: 3px solid blue;
+const StyledCounter = styled(Counter)`
+  background-color: ${openProps.gray0};
+  color: ${openProps.teal4};
+  font-size: ${openProps.fontSize6};
+  border: ${openProps.borderSize3} solid ${openProps.indigo7};
+  border-radius: ${openProps.radius3};
 `;
 
 const StyledPWithAdditionalProps = styled("p")<{
   borderColor: "red" | "green" | "blue";
 }>`
-  border: 3px solid ${(p) => p.borderColor};
+  border: 3px solid ${openProps.indigo7};
+  background-color: ${openProps.gray0};
+  font-size: ${openProps.fontSize4};
+  font-weight: ${openProps.fontWeight7};
+  color: ${(p) => p.borderColor};
+  text-align: center;
+  border-radius: ${openProps.radius2};
+  padding: .5rem 1rem;
 `;
 
 const StyledH1 = styled("h1")`
+  padding: .5rem 1rem;
+  background-color: ${openProps.gray0};
+  color: ${openProps.red5};
   font-size: ${openProps.fontSize6};
+  border: ${openProps.borderSize3} solid ${openProps.indigo7};
+  border-radius: ${openProps.radius3};
+`;
+const CasablancaSpan = styled("span")`
+  color: ${openProps.teal4};
+`;
+const ReactSpan = styled("span")`
+  color: ${openProps.blue4};
 `;
 
-const fontSize = css`
-  font-size: 3rem;
+const description = css`
+  font-size: ${openProps.fontSize4};
+  font-weight: ${openProps.fontWeight7};
+  color: ${openProps.gray1};
+  text-align: center;
+`;
+
+const ColorChangeButton = styled("button")`
+  background-color: ${openProps.gray0};
+  font-size: ${openProps.fontSize4};
+  border: ${openProps.borderSize3} solid ${openProps.indigo7};
+  border-radius: ${openProps.radius3};
+`;
+
+const PanelDiv = styled("div")`
+  display: grid;
+  grid-template-columns: 1fr 1fr;
+  column-gap: 1rem;
+  & > div{
+    display: grid;
+    row-gap: 3px;
+  }
 `;
 
 const ContainerDiv = styled("div")`
+  min-height: 100svh;
   display: grid;
+  padding: .5rem 0;
   grid-template-columns: auto;
+  row-gap: 5px;
   justify-content: center;
-  align-items: center;
-  & > .${StyledPWithAdditionalProps} {
-    text-align: center;
-  }
+  align-content: start;
+  background-image: ${openProps.gradient7};
 `;
 
 export const App: FC = () => {
   const [color, setColor] = useState<"red" | "green" | "blue">("red");
   return (
     <ContainerDiv>
-      <StyledH1>Casablanca + React-TS</StyledH1>
-      <p>Zero runtime CSS-in-JS for vite.</p>
-      <StyledButton />
-      <StyledPWithAdditionalProps borderColor={color}>
-        Dynamic style
-      </StyledPWithAdditionalProps>
-      <button
-        type="button"
-        onClick={() =>
-          setColor((prev) =>
-            prev === "red" ? "blue" : prev === "blue" ? "green" : "red",
-          )
-        }
-      >
-        ChangeColor
-      </button>
-      <p className={fontSize}>Foobar</p>
+      <StyledH1>
+        <CasablancaSpan>Casablanca</CasablancaSpan>
+        {" + "}
+        <ReactSpan>React-TS</ReactSpan>
+      </StyledH1>
+      <p className={description}>Zero runtime CSS-in-JS for vite.</p>
+      <PanelDiv>
+        <StyledCounter />
+        <div>
+          <StyledPWithAdditionalProps borderColor={color}>
+            Dynamic style
+          </StyledPWithAdditionalProps>
+          <ColorChangeButton
+            type="button"
+            onClick={() =>
+              setColor((prev) => {
+                switch (prev) {
+                  case "red": {
+                    return "green";
+                  }
+                  case "green": {
+                    return "blue";
+                  }
+                  case "blue": {
+                    return "red";
+                  }
+                  default: {
+                    throw new Error("Unreachable");
+                  }
+                }
+              })
+            }
+          >
+            Change Color
+          </ColorChangeButton>
+        </div>
+      </PanelDiv>
     </ContainerDiv>
   );
 };
