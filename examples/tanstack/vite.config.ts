cf23e18e8718daf2824122e29f936098749b6e39
T. Kameyama <tkamenoko@vivaldi.net>
2025-12-17 16:21:51 +0900

    docs: add tanstack example [WIP]

---
+++ examples/tanstack/vite.config.ts
@@ -0,0 +1,22 @@
+import { casablanca } from "@casablanca-css/core/vite";
+import { casablancaStyled } from "@casablanca-css/styled/vite";
+import { tanstackStart } from "@tanstack/react-start/plugin/vite";
+import react from "@vitejs/plugin-react";
+import postcssNested from "postcss-nested";
+import { defineConfig } from "vite";
+import tsconfigPaths from "vite-tsconfig-paths";
+
+export default defineConfig({
+  css: {
+    postcss: { plugins: [postcssNested()] },
+    devSourcemap: true,
+  },
+  server: {},
+  plugins: [
+    tsconfigPaths(),
+    tanstackStart({ customViteReactPlugin: true }),
+    react(),
+    casablancaStyled(),
+    casablanca({ evaluateOptions: { globals: {} } }),
+  ],
+});
